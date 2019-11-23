379d8a1645ddde280439afad3e14e07adb006850
Thuong Ho <thuong.t.ho@gmail.com>
2019-11-23 15:35:12 -0800

    Updates

---
+++ precache-manifest.717b64336397e70bf6e70ac0374cedcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f194c4a120171d339b5174f0d1e86b5c",
+    "url": "/monsters-dex/index.html"
+  },
+  {
+    "revision": "af8b022ec0109f5be7b5",
+    "url": "/monsters-dex/static/css/main.f7bf9ff6.chunk.css"
+  },
+  {
+    "revision": "2fcc140a7421effa832c",
+    "url": "/monsters-dex/static/js/2.83dd2ab9.chunk.js"
+  },
+  {
+    "revision": "af8b022ec0109f5be7b5",
+    "url": "/monsters-dex/static/js/main.c0a5b2c5.chunk.js"
+  },
+  {
+    "revision": "04ad7e2aa5abd04d6ef1",
+    "url": "/monsters-dex/static/js/runtime-main.c3b24e9b.js"
+  }
+]);
